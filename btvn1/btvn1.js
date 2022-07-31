01d60588cde62e65ea43d7c3ae75f218e79ad519
nguyenthanhchung090302 <110025385+nguyenthanhchung090302@users.noreply.github.com>
2022-07-31 23:21:35 +0700

    bài tập về nhà

---
+++ btvn1/btvn1.js
@@ -0,0 +1,117 @@
+// Bài 1: nhập vào hai số a và b, và kiểm tra xem a có chia hết cho b không? 
+
+// let a = Number(prompt("nhập số a"));
+// let b = Number(prompt(" nhập số b"));
+
+// console.log(c = a/b);
+
+
+// Bài 2: nhập tuổi và in ra kết quả nếu tuổi học sinh đó k đủ điều kiện vào học lớp 10.
+
+// let a = Number(prompt("mời bạn nhập tuổi "));
+// if(a >= 15){
+//     alert("đủ điều kiện vào lớp 10")
+// }else{
+//     alert(" không đủ điều kiện vào lớp 10")
+// }
+
+// Bài 3: Nhập một số nguyên bất kỳ và in kết quả ra màn hình 
+//để nói cho người dùng biết số đó là lớn hay nhỏ hơn 0
+
+// let a = Number(prompt("mời bạn nhập số "));
+// let b = 0;
+// if( a > b){
+//     alert("lớn hơn 0")
+// }else{
+//     alert("nhỏ hơn 0")
+// }
+ 
+// Bài 4 :nhập 3 số nguyên và tìm giá trị lớn nhất của 3 số đó.
+//  let a = Number(prompt("mời nhập số a"));
+//  let b = Number(prompt("mời nhập số b"));
+//  let c = Number(prompt("mời nhập số c"));
+//  console.log(Math.max(a, b, c));
+
+// BÀI 5: Xếp hạng học lực của học sinh dựa trên các điểm bài kiểm tra, điểm thi giữa kỳ, điểm thi cuối kỳ
+
+// let a = Number(prompt("nhập điểm bài kiểm tra"));
+// let b = Number(prompt("nhập điểm thi giữa kì"));
+// let c = Number(prompt("nhập điểm thi cuối kì"));
+// let d =((10/100)*a + (20/100)*b +(70/100)*c);
+// if(d >= 8){
+//     alert("học lực giỏi")
+// }else if(d > 6.5 && d < 8){
+//     alert("học lực khá")
+// }else if( d > 5 && d < 6.5){
+//     alert("học lực trung bình")
+// }else{
+//     alert("học lực kém")
+// }
+ 
+ // bài 6 
+
+//  let a = Number(prompt("Nhập tổng doanh số:"));
+// if ( a > 0 && a <= 5000000){
+//     alert(`Số hoa hồng là ${a * 5 /100}`);
+//  } else if (5000000 <= a && a < 10000000){
+//      alert(`Số hoa hồng là ${(5000000 * 5 /100) +((a-5000000)* 10 /100)}`);
+// } else if (10000000 <= a && a < 18000000 ){
+//      alert(`Số hoa hồng là ${(5000000 * 5 /100) +(5000000* 10 /100) +(a - 10000000)*15/100}`);
+//  } else if (a<= 18000000){
+//     alert(`Số tiền hoa hồng là ${(5000000 * 5 /100) +(5000000* 10 /100)+
+//     (8000000*10/100)+((a-18000000)*20/100)}`);
+//  }
+ 
+ 
+// bài 7 đề bài dài quá
+
+// let a = Number(prompt("nhập chiều cao của bạn"));
+// let b = Number(prompt("nhập cân nặng của bạn"));
+// let c =(b/(a*a));
+// if(c <= 18){
+//     alert("chúc mừng bạn bị thiếu cân")
+// }else if(c > 18 && c < 25){
+//     alert("bạn bình thường")
+// }else{
+//     alert("bạn thừa cân")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
